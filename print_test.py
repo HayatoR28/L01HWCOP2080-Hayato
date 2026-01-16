e4ddc4dccb582de20c2fc3add4e5972e939b41b0
Hayato Rodriguez <hrodriguez8956@floridapoly.edu>
2026-01-16 17:20:36 +0000

    Finished print test

---
+++ print_test.py
@@ -0,0 +1,12 @@
+my_name = "Hayato Rodriguez"
+
+print("Student Name:", my_name)
+
+print("PEP 8 Indentation Rule: Use 4 spaces per indentation level.")
+
+print("Python Language Fact: Python source code is executed by an interpreter.")
+
+print("Course Outcomes I Hope to Gain:")
+print("1. Improve my problem-solving and coding skills.")
+print("2. Learn how to write clean and readable Python code.")
+print("3. Gain more skills in debugging and understanding programs.")
