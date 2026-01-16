eaf060a9c8da612f72af2f832f558a6b307fdaac
Hayato Rodriguez <hrodriguez8956@floridapoly.edu>
2026-01-16 17:34:48 +0000

    Completed practice

--- print_test.py
+++ print_test.py
@@ -6,7 +6,4 @@ print("PEP 8 Indentation Rule: Use 4 spaces per indentation level.")
 
 print("Python Language Fact: Python source code is executed by an interpreter.")
 
-print("Course Outcomes I Hope to Gain:")
-print("1. Improve my problem-solving and coding skills.")
-print("2. Learn how to write clean and readable Python code.")
-print("3. Gain more skills in debugging and understanding programs.")
+print("Improve my problem-solving and coding skills, Learn how to write clean and readable Python code, and Gain more skills in debugging and understanding programs.")
